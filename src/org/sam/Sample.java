13073ddc429098107d96f5b01e9467537ed20346
sunmathi23 <sunsadhana23@gmail.com>
2021-10-11 07:00:04 +0530

    Even numbers

---
+++ src/org/sam/Sample.java
@@ -0,0 +1,22 @@
+package org.sam;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		int i=10;
+		
+		{
+			if (i % 2 == 0)
+			{
+				System.out.println("even number");
+				
+			}
+			else
+			{
+				System.out.println("odd number");
+			}
+			
+		}
+	}
+}
